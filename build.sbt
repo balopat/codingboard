2b410f4026ea9e26e2d1c30bcc753301c1b3e642
balopat <balopat@balopat.com>
2012-12-09 16:10:45 +0000

    using jade as templating engine, passing some parameters, basic experimenting

--- build.sbt
+++ build.sbt
@@ -16,5 +16,6 @@ libraryDependencies ++= Seq(
   "org.scalatra" % "scalatra-specs2" % "2.1.1" % "test",
   "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "container",
-  "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
-)
+  "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet",
+  "jar", "jar")),
+  "org.fusesource.scalamd" % "scalamd" % "1.5")
--- src/main/scala/com/balopat/dojoshare/DojoShareServlet.scala
+++ src/main/scala/com/balopat/dojoshare/DojoShareServlet.scala
@@ -5,14 +5,29 @@ import scalate.ScalateSupport
 
 class DojoShareServlet extends ScalatraServlet with ScalateSupport {
 
+
+
   get("/") {
+    contentType="text/html"
+    jade("index")
+  }
+
+  post("/submitroom") {
     <html>
       <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">belloka to Scalate</a>.
-      </body>
-    </html>
+          Variables: {params("room")}
+        </body>
+      </html>
   }
+
+  get("/rooms/:room") {
+
+    
+    contentType="text/html"
+    jade("room", "room" -> params("room"))
+    
+  }
+
 
   notFound {
     // remove content type in case it was set through an action
