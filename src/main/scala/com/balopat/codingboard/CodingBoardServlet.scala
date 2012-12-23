d6d67768a72b3fc967ff39fdac4754287b82275b
balopat <balopat@balopat.com>
2012-12-23 19:47:55 +0100

    refactoring servlet to move all function definitions to Helper class

--- src/main/scala/com/balopat/codingboard/CodingBoardServlet.scala
+++ src/main/scala/com/balopat/codingboard/CodingBoardServlet.scala
@@ -1,35 +1,14 @@
 package com.balopat.codingboard
 
-import org.scalatra._
-import scalate.ScalateSupport
-import scala.collection.immutable.List
-import org.scalatra.json.{JValueResult, JacksonJsonSupport}
-import org.json4s._
-import JsonDSL._
-import actors.{Actor,TIMEOUT}
-import Actor._
-
-
-class CodingBoardServlet(boards: CodingBoards = CodingBoards.instance) extends ScalatraServlet
-  with ScalateSupport with JValueResult
-  with JacksonJsonSupport 
+class CodingBoardServlet(boards: CodingBoards = CodingBoards.instance) extends CodingBoardViewHelper 
   {  
-
-    implicit protected val jsonFormats: Formats = DefaultFormats
 
     get("/") {
        index()
     }
 
     post("/submitboard") {
-         val board = params("board")
-         val lengthOfSession = params("lengthOfSessionInMinutes")
-         val validationErrors = boards.validate(board, lengthOfSession)
-         if (!validationErrors.isEmpty) {
-             contentType="text/html"
-             jade("createboard", (validationErrors :+ ("board"->board) :+ ("lengthOfSession" -> lengthOfSession)).toArray :_* )
-         } else joinCodingBoard(boards.create(board, lengthOfSession.toInt * 60000, System.currentTimeMillis).url)
-         
+       createAndJoinBoard(params("board"), params("lengthOfSessionInMinutes")) 
     }
 
     get("/boards/:board") {
@@ -41,75 +20,19 @@ class CodingBoardServlet(boards: CodingBoards = CodingBoards.instance) extends S
     }
 
     post("/boards/:board/post") {
-       val board = params("board")
-       val formToken = params("formtoken")
-       whenCodingBoardExistsOtherwiseErrorOnHomePage(
-            board, 
-            { 
-               val codeSnippet = 
-                  new CodeSnippet(formToken, 
-                       params("description"), 
-                       params("code"), 
-                       params("language"), 
-                       System.currentTimeMillis)
-               boards.get(board) += codeSnippet
-               joinCodingBoard(board )
-            }
-       )
+       postSnippetToBoard(params("board"), params("formtoken"), params("description"), params("code"), params("language"))
     }
 
     post("/boards/:board/refresh") {
-      val board = params("board")
-       if (boardChanged(board, params("lastCodeSnippetId"))) {
-          compact(render(codeSnippets(board).last.toJSON))
-       } else {
-          compact(render("refresh"->"norefresh"))
-       }
+       isBoardChangedSinceLastCodeSnippet(params("board"), params("lastCodeSnippet"))
     }
-
-    def boardChanged(board: String, lastCodeSnippetId: String) = {
-       (!codeSnippets(board).isEmpty) && (!codeSnippets(board).last.id.equals(lastCodeSnippetId))
-    }
-  
-    def codeSnippets(board: String) = boards.get(board).codeSnippets
 
     get("/boards/:board/codesnippet") {
-       val board = params("board")
-       contentType="text/html"
-       whenCodingBoardExistsOtherwiseErrorOnHomePage(
-            board, 
-            jade("codesnippet", 
-                   "formtoken" -> java.util.UUID.randomUUID.toString,  
-                   "board" -> board)
-       )
+       postCodeSnippetTo(params("board"))
     }
-       
-    def index(extraAttributes: (String, Any)*) = {
-       contentType="text/html"
-       jade("index", ("boards" -> boards.list :: extraAttributes.toList).toArray: _*)
-    }
-
-    def joinCodingBoard(boardURL: String, extraAttributes: (String, Any)*) = {
-       contentType="text/html"
-       whenCodingBoardExistsOtherwiseErrorOnHomePage(
-          boardURL, 
-          jade("board", ("board" -> boards.get(boardURL) :: extraAttributes.toList).toArray: _*  )
-       )
-    }
-   
-    def whenCodingBoardExistsOtherwiseErrorOnHomePage(boardURL: String, boardExistsPage: =>Any) = {
-       if (boards.exists(boardURL)) {
-          boardExistsPage
-       } else {
-          index("errorMessage" -> "CodingBoard not found!")
-       }
-    }
-
     
-  notFound {
-     // remove content type in case it was set through an action
+    notFound {
      contentType = null
-     // Try to render a ScalateTemplate if no route matched
      findTemplate(requestPath) map { path =>
         contentType = "text/html"
         layoutTemplate(path)
