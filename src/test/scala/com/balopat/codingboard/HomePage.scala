62b9e99faaf5f7df48bf7de7ce5ee9fa0065e3ea
carlosvillademor <carlos.villademor@gmail.com>
2013-02-09 15:53:55 +0000

    Added back the HomePage functional test. Using pageTitle instead of deprecated title

---
+++ src/test/scala/com/balopat/codingboard/HomePage.scala
@@ -0,0 +1,19 @@
+package com.balopat.codingboard
+
+import org.scalatest._
+import org.scalatest.selenium.Chrome
+
+class HomePage extends FlatSpec with ShouldMatchers with Chrome {
+
+  "The CodingBoard homepage" should "have the correct title" in {
+    go to ("http://localhost:8080/")
+    pageTitle should be ("CodingBoard")
+  }
+
+  "The CodingBoard homepage" should "have a button to create board" in {
+    go to ("http://localhost:8080/")
+    click on id("goto_create_board")
+    pageTitle should be ("CodingBoard - Create Board")
+  }
+
+}
