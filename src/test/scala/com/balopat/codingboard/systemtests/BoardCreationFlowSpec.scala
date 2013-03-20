42d1dab737be08fdaa882afdc9f7e335c3c763ed
carlosvillademor <carlos.villademor@gmail.com>
2013-03-20 23:20:26 +0000

    Merging systemtests

--- src/test/scala/com/balopat/codingboard/systemtests/BoardCreationFlowSpec.scala
+++ src/test/scala/com/balopat/codingboard/systemtests/BoardCreationFlowSpec.scala
@@ -1,15 +1,25 @@
 package com.balopat.codingboard.systemtests
 
 import org.scalatest._
+import concurrent.{AsyncAssertions, Eventually}
 import org.scalatest.selenium.Chrome
+import org.scalatest.time.SpanSugar._
 
-class BoardCreationFlowSpec extends FlatSpec with ShouldMatchers with Chrome {
+class BoardCreationFlowSpec extends FlatSpec with ShouldMatchers with Eventually with AsyncAssertions with Chrome {
 
   class HomePage extends Page {
     val url = "http://localhost:8080"
   }
 
   val homePage = new HomePage()
+
+  def createBoard(boardName: String){
+    go to homePage
+    click on "goto_create_board"
+    textField("board").value = boardName
+    textField("lengthOfSessionInMinutes").value = "1"
+    click on "submit"
+  }
 
   "The CodingBoard homepage" should "have the correct title" in {
     go to homePage
@@ -27,12 +37,26 @@ class BoardCreationFlowSpec extends FlatSpec with ShouldMatchers with Chrome {
   }
 
   "The CreateBoard page " should "lead to the Test Board when submitted and have a filter field" in {
-    textField("board").value = "Test Board"
-    textField("lengthOfSessionInMinutes").value = "1"
-    click on id("submit")
+    createBoard("Test Board")
     pageTitle should be("CodingBoard - Test Board")
     go to homePage
     textField("filter").value should be ("")
+  }
+
+  "The CodingBoard homepage" should "display only the boards matching the filter expression" in {
+    createBoard("New Board 1")
+    createBoard("New Board 2")
+    createBoard("Different Board")
+    go to ("http://localhost:8080/")
+    findAll(new CssSelectorQuery("table.table.table-hover.striped td.board-name")).length shouldBe 4
+    textField("filter").value = "Different"
+    eventually(timeout(1 seconds), interval(5 millis)) {
+      findAll(new CssSelectorQuery("table.table.table-hover.striped tr.clickable[style='display: table-row;']")).length shouldBe 1
+      find(new CssSelectorQuery("table.table.table-hover.striped tr.clickable[style='display: table-row;'] td.board-name")).get.text should be ("Different Board")
+    }
+    textField("filter").value = ""
+    findAll(new CssSelectorQuery("table.table.table-hover.striped td.board-name")).length shouldBe 4
     close()
   }
+
 }
