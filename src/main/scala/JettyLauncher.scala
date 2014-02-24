3ea9906aa598887c90685fe68b0c7d5fcb2cab32
balopat <balopat@balopat.com>
2014-02-24 00:20:19 -0500

    setting up Samebug filter

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,4 +1,7 @@
+import com.samebug.notifier.servlet.SamebugFilter
 import com.samebug.notifier.{SamebugUncaughtExceptionHandler, Samebug}
+import java.util
+import javax.servlet.DispatcherType
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.webapp.WebAppContext
@@ -20,6 +23,7 @@ object JettyLauncher {
     context.setResourceBase("src/main/webapp")
     context.addServlet(new ServletHolder(new CodingBoardServlet()), "/*")
     context.addServlet(classOf[DefaultServlet], "/")
+    context.addFilter(classOf[SamebugFilter], "/*", util.EnumSet.allOf(classOf[DispatcherType]))
 
     server.setHandler(context)
 
